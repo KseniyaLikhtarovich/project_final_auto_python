e8010117b152ce8c4df6460b55326e33b08a77fc
KseniyaLikhtarovich <51284677+KseniyaLikhtarovich@users.noreply.github.com>
2021-10-05 19:18:12 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,11 +6,13 @@ import mysql.connector as mysql
 # Драйвер
 @pytest.fixture(scope="module")
 def browser():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=chrome_options)
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.add_argument('--no-sandbox')
+#     chrome_options.add_argument('--headless')
+#     chrome_options.add_argument('--disable-gpu')
+#     driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome()
+    driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
